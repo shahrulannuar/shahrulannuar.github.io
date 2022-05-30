1531c53813341fcfebbc88f682e761bafe9053d9
shahrulannuar <106234691+shahrulannuar@users.noreply.github.com>
2022-05-30 08:42:37 +0800

    Update projects.js

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,30 +1,5 @@
 const projects = [
     {
-        "name": "MY Chat",
-        "description": "An online chatting website with a beautiful UI and the ability to create your own chat rooms! Made originally for a hackathon",
-        "url": "https://"
-    },
-    {
-        "name": "TODO List",
-        "description": "An Online Todo List with localStorage option to save your tasks on one device only and a Login system to save them globally!",
-        "url": "https://"
-    },
-    {
-        "name": "Code Bin",
-        "description": "Store and Access your codes on the cloud",
-        "url": "https://"
-    },
-    {
-        "name": "MY Cat API",
-        "description": "The Only API You'll Ever Need! Has Tons of Image Manipulation and Utility Endpoints!",
-        "url": "https://"
-    },
-    {
-        "name": "Instagram User Profiles",
-        "description": "A very beautiful web page for viewing info for an instagram user!",
-        "url": "https://"
-    },
-    {
         "name": "URL Shortener",
         "description": "A simple, easy to use URL shortener with statistics on the number of clicks on your endpoint",
         "url": "https://"
