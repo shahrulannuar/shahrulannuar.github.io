c575d4e240628166b82e8b7d1b1deac2651a154a
shahrulannuar <106234691+shahrulannuar@users.noreply.github.com>
2022-05-30 08:28:08 +0800

    Update projects.js

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -2,36 +2,36 @@ const projects = [
     {
         "name": "Pop Chat",
         "description": "An online chatting website with a beautiful UI and the ability to create your own chat rooms! Made originally for a hackathon",
-        "url": "https://hackathon-chat.popcat.xyz"
+        "url": "https://"
     },
     {
         "name": "TODO List",
         "description": "An Online Todo List with localStorage option to save your tasks on one device only and a Login system to save them globally!",
-        "url": "https://popcat.xyz/todo"
+        "url": "https://"
     },
     {
         "name": "Code Bin",
         "description": "Store and Access your codes on the cloud",
-        "url": "https://code.popcat.xyz"
+        "url": "https://"
     },
     {
         "name": "Pop Cat API",
         "description": "The Only API You'll Ever Need! Has Tons of Image Manipulation and Utility Endpoints!",
-        "url": "https://popcat.xyz/api"
+        "url": "https://"
     },
     {
         "name": "Instagram User Profiles",
         "description": "A very beautiful web page for viewing info for an instagram user!",
-        "url": "https://popcat.xyz/instagram"
+        "url": "https://"
     },
     {
         "name": "URL Shortener",
         "description": "A simple, easy to use URL shortener with statistics on the number of clicks on your endpoint",
-        "url": "https://url.popcat.xyz"
+        "url": "https://"
     },
     {
         "name": "Pop Cat Wrapper - NPM Package",
         "description": "An NPM Package To Make Your Life Easier While Using The Pop Cat API!",
-        "url": "https://npmjs.com/popcat-wrapper"
+        "url": "https://"
     }
 ]
